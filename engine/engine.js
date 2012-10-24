d71e19dcdbc459563620dca25493c73b1af0e5bb
Arjan Kuijpers <arjankuijpers@live.nl>
2012-10-24 23:49:22 +0200

    added max his Final asset edited arjan's asset (credits)

--- engine/engine.js
+++ engine/engine.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 //Engine QuestionJS & Game Developed By Createc
-var version = "Game v05084 ,QJS integrated"
+var version = "Game v05085 ,QJS integrated"
 
 var debug = 1;
 
